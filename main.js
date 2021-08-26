6cf06447d68aee380a3d673a4c82768fa8547619
Sumedh Inamdar <sinamdar@gmail.com>
2021-08-25 23:23:20 -0700

    inital UI commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let userScore = 0;
+let compScore = 0;
+let round = 0;
+
 function computerPlay() {
     let play = ['Rock', 'Paper', 'Scissors'];
     let select = Math.floor(3*Math.random());
@@ -5,11 +9,6 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    /*
-    1. Standardize playerSelection input (first letter capitalized, all remaining are lowercase)
-    2. Check if player input is valid (Rock, Paper, Scissors). throw an error if not.
-    3. Check user's input and return string based on computerSelection using switch statement
-    */
    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
    validInputs = ['Rock', 'Paper', 'Scissors'];
    lookUpTable = [[0, -1, 1], [1, 0, -1], [-1, 1, 0]]; // 0 = tie; 1 = win; 2 = lose. [[Rock user input] [Paper user input] [Scissors user input]]
@@ -33,22 +32,27 @@ function playRound(playerSelection, computerSelection) {
    }
 }
 
-function game() {
-    let score = 0;
-    let round = 1;
-    let roundResult;
-    
-    while (round <= 5) {
-        userInput = prompt('Type your pick below (Rock, Paper, or Scissors)');
-        roundResult = playRound(userInput, computerPlay());
-        if (roundResult.includes('invalid')) {
-            console.log(roundResult);
-            continue;
-        }
-        console.log(`Round ${round}: ${roundResult}`);
-        score = roundResult.includes('Win') ? score + 1 : roundResult.includes('Lose') ? score - 1 : score;
-        round = roundResult.includes('Tie') ? round : round + 1;
+function updateScore(playResult) {
+    if (playResult.includes('Win')) {
+        userScore++;
+    } else if (playResult.includes('Lose')) {
+        compScore++;
     }
 
-    console.log(`and the winner is....${score > 0 ? 'You! Good job!' : 'the Computer :( next time!'}`);
+    
+
 }
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button => {
+    button.addEventListener('click', e => {
+        const playResult = playRound(e.id, computerPlay());
+        document.querySelector('.playResultContainer').textContent = playResult;
+        updateScore(playResult);
+    });
+});
+
+
+
+
